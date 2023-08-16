99e01ee94069b2c4573e2ad2f614fa5ac79cad84
Kianny G. Marín <132239147+kiannygmarin@users.noreply.github.com>
2023-08-16 23:00:01 +0000

    listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState("")
+	const [lista, setLista] = useState(["lavar el carro", "ir al cine"])
+
+	const handleInput = (e) => {
+		let texto = e.target.value
+		if (e.keyCode == 13) {
+			setTarea(texto)
+			setLista([...lista, texto])
+		}
+	}
+
+	const deleteTask = (index) => {
+		let tempArr = lista.slice()
+		tempArr = tempArr.filter((item, index2) => { return index2 != index })
+		setLista(tempArr)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="text-center">
+				{/* <input placeholder="Escriba aquí una tarea"
+					onKeyUp={
+						(e) => { handleInput(e) }
+					}
+
+				/> */}
+
+			</div>
+			<div className="container md mt-3 pt-5" style={{width: "18rem"}}>
+			<input placeholder="Escriba aquí una tarea"
+					onKeyUp={
+						(e) => { handleInput(e) }
+					}
+
+				/>
+				<div>
+				La lista de tareas es:
+				</div>
+				<ul className="list-group list-group-numbered shadow p-3 mb-5 bg-body rounded">
+					{
+						lista && lista.length > 0 ?
+							<>{
+								lista.map((item, index) => {
+									return <li key={index}>
+										{item}
+										<button type="button" onClick={e => { deleteTask(index) }}>
+											❌
+										</button>
+									</li>
+								})
+							}</>
+							: "la lista está vacía"
+					}
+				</ul>
+			</div>
+			<div>
+				{/* La lista de tareas es:
+				<ul className="list-group list-group-numbered shadow p-3 mb-5 bg-body rounded">
+					{
+						lista && lista.length > 0 ?
+							<>{
+								lista.map((item, index) => {
+									return <li key={index}>
+										{item}
+										<button type="button" onClick={e => { deleteTask(index) }}>
+											❌
+										</button>
+									</li>
+								})
+							}</>
+							: "la lista está vacía"
+					}
+				</ul> */}
+			</div>
+		</>
 	);
 };
 
